a05151fbc54472f3710fd7e3c35980874cd3944f
Raees Ahamed <raeesahmedh116@gmail.com>
2024-07-15 00:14:32 +0530

    Db Connection

--- project Source code/php/classes/db_connection.php
+++ project Source code/php/classes/db_connection.php
@@ -1,34 +1,13 @@
 <?php
-class DbConnection
-{
-    private $servername = "localhost";
-    private $username = "unihub";
-    private $password = "Unihub@1234";
-    private $dbname = "wellassaunihub";
-    private $conn;
+$servername = "localhost";
+$username = "unihub";
+$password = "Unihub@1234";
+$dbname = "wellassaunihub";
 
-    // Method to create the database connection
-    private function connect()
-    {
-        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-        // Check the connection
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-
-    // Method to get the connection
-    public function getConnection()
-    {
-        return $this->conn;
-    }
-
-    // Method to close the connection
-    public function close()
-    {
-        if ($this->conn) {
-            $this->conn->close();
-        }
-    }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
