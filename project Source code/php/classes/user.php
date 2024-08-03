316a91178bce5b455034e660c6ba68ec08153706
sajith-ali <sajithalise@gmail.com>
2024-08-04 03:21:43 +0530

    freelance u[date & new page
    freelance_view.php
    user.php
    user_profile.php

--- project Source code/php/classes/user.php
+++ project Source code/php/classes/user.php
@@ -1,5 +1,5 @@
 <?php
-	
+
 class User
 {
     private $userId;
@@ -15,11 +15,12 @@ class User
     private $productId;
     private $dbconn;
 
-    public function __construct() {
-
+    public function __construct()
+    {
     }
     //constructor for sarath products
-    public static function constructSPWithProductId($dbconn, $userId, $productId) {
+    public static function constructSPWithProductId($dbconn, $userId, $productId)
+    {
         $user = new self();
         $user->dbconn = $dbconn;
         $user->userId = $userId;
@@ -28,14 +29,16 @@ class User
         return $user;
     }
 
-    public static function constructSPWithUserId($dbconn, $userId) {
+    public static function constructSPWithUserId($dbconn, $userId)
+    {
         $user = new self();
         $user->dbconn = $dbconn;
         $user->userId = $userId;
         $user->loadUserSP();
         return $user;
     }
-    public static function constructCUSWithUserId($dbconn, $userId) {
+    public static function constructCUSWithUserId($dbconn, $userId)
+    {
         $user = new self();
         $user->dbconn = $dbconn;
         $user->userId = $userId;
@@ -44,7 +47,8 @@ class User
     }
 
 
-    private function loadUserCUS() {
+    private function loadUserCUS()
+    {
         $stmt = $this->dbconn->prepare("SELECT * FROM user WHERE user_id = ?");
         $stmt->bind_param("s", $this->userId);
         $stmt->execute();
@@ -58,7 +62,8 @@ class User
         $this->profileImage = $user['profile_photo'];
     }
 
-    private function loadUserSP() {
+    private function loadUserSP()
+    {
         $stmt = $this->dbconn->prepare("SELECT * FROM user WHERE user_id = ?");
         $stmt->bind_param("s", $this->userId);
         $stmt->execute();
@@ -74,95 +79,114 @@ class User
         $this->profileImage = $user['profile_photo'];
     }
 
-    public function getProfileImage() {
+    public function getProfileImage()
+    {
         return $this->profileImage;
     }
 
-    public function getFirstName() {
+    public function getFirstName()
+    {
         return $this->firstName;
     }
 
-    public function getLastName() {
+    public function getLastName()
+    {
         return $this->lastName;
     }
-    public function getBusinessName() {
+    public function getBusinessName()
+    {
         return $this->businessName;
     }
 
-    public function getPhone() {
+    public function getPhone()
+    {
         return $this->phone;
     }
 
-    public function getWphone() {
+    public function getWphone()
+    {
         return $this->wphone;
     }
-    public function getEmail() {
+    public function getEmail()
+    {
         return $this->email;
     }
 
-    public function getAddress() {
+    public function getAddress()
+    {
         return $this->address;
     }
 
-    public function getDescription() {
+    public function getDescription()
+    {
         return $this->description;
     }
 
-    public function setFirstName() {
+    public function setFirstName()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET first_name=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->firstName, $this->userId);
         $stmt->execute();
     }
 
-    public function setLastName() {
+    public function setLastName()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET last_name=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->lastName, $this->userId);
         $stmt->execute();
     }
 
-    public function setBusinessName() {
+    public function setBusinessName()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET business_name=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->businessName, $this->userId);
         $stmt->execute();
     }
 
-    public function setPhone() {
+    public function setPhone()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET contact_number=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->phone, $this->userId);
         $stmt->execute();
     }
 
-    public function setWphone() {
+    public function setWphone()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET whatsapp_number=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->wphone, $this->userId);
         $stmt->execute();
     }
 
-    public function setEmail() {
+    public function setEmail()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET email=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->email, $this->userId);
         $stmt->execute();
     }
 
-    public function setAddress() {
+    public function setAddress()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET service_address=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->address, $this->userId);
         $stmt->execute();
     }
 
-    public function setDescription() {
+    public function setDescription()
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET description=? WHERE user_id=?");
         $stmt->bind_param("ss", $this->description, $this->userId);
         $stmt->execute();
     }
     //remember we need to dynamically change the image url to a specific path
-    public function setProfileImage($profileImage) {
+    public function setProfileImage($profileImage)
+    {
         $stmt = $this->dbconn->prepare("UPDATE user SET profile_photo=? WHERE user_id=?");
         $stmt->bind_param("ss", $profileImage, $this->userId);
         $stmt->execute();
     }
 
-    public function getPhotos() {
+    public function getPhotos()
+    {
         $stmt = $this->dbconn->prepare("SELECT * FROM image WHERE user_id = ?");
         $stmt->bind_param("s", $this->userId);
         $stmt->execute();
@@ -174,12 +198,51 @@ class User
         return $photos;
     }
     //for sarath products images
-    public function getProductId() {
+    public function getProductId()
+    {
         // i think we have to give the prouduct id with url when click the product
         // so we definitely need to parse an argument to the function.
         // for now i just pass the product id as a parameter from url  
     }
-    public function getProductPhotos() {
+
+    //i plan this getallproducts function to be used for sarath products on the userprofile page
+    public function getAllProducts()
+    {
+        $stmt = $this->dbconn->prepare("SELECT * FROM product WHERE provider_id = ? AND product_id = ?");
+        $stmt->bind_param("ss", $this->userId, $this->productId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $products = [];
+        while ($product = $result->fetch_assoc()) {
+            $products[] = $product;
+        }
+        return $products;
+    }
+
+    //consider the rating
+    public function getProductRating()
+    {
+        $stmt = $this->dbconn->prepare("SELECT * FROM rating WHERE user_id = ? AND product_id = ?");
+        $stmt->bind_param("ss", $this->userId, $this->productId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rating = $result->fetch_assoc();
+
+        return $rating;
+    }
+
+    public function getRating()
+    {
+        $stmt = $this->dbconn->prepare("SELECT * FROM rating WHERE user_id = ?");
+        $stmt->bind_param("ss", $this->userId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rating = $result->fetch_assoc();
+
+        return $rating;
+    }
+    public function getProductPhotos()
+    {
         $stmt = $this->dbconn->prepare("SELECT * FROM image WHERE user_id = ? AND product_id = ?");
         $stmt->bind_param("ss", $this->userId, $this->productId);
         $stmt->execute();
@@ -191,17 +254,17 @@ class User
         return $photos;
     }
 
-    public function addPhoto($photoPath) {
+    public function addPhoto($photoPath)
+    {
         $stmt = $this->dbconn->prepare("INSERT INTO image (user_id, image_path) VALUES (?, ?)");
         $stmt->bind_param("ss", $this->userId, $photoPath);
         $stmt->execute();
     }
 
-    public function deletePhoto($photoPath) {
+    public function deletePhoto($photoPath)
+    {
         $stmt = $this->dbconn->prepare("DELETE FROM photos WHERE image_path = ? AND user_id = ?");
         $stmt->bind_param("ss", $photoPath, $this->userId);
         $stmt->execute();
     }
 }
-
-?>
--- project Source code/user_profile.php
+++ project Source code/user_profile.php
@@ -2,6 +2,9 @@
 include_once('./php/classes/db_connection.php');
 include_once('./php/classes/user.php');
 session_start();
+//remember this
+//$_SESSION['user_id'] = "";
+//$_SESSION['user_type'] = "";
 
 $db = new DBConnection();
 $userId = $_GET['userId'];
@@ -9,14 +12,19 @@ if (!empty($_GET['productId'])) {
     $productId = $_GET['productId'];
 }
 
-if (isset($productId)) {
+if ($_SESSION['user_type'] == "customer") {
     $dbconn = $db->getConnection();
-    $user = User::constructSPWithProductId($dbconn, $userId, $productId);
-    $photos = $user->getProductPhotos();
+    $user = User::constructCUSWithUserId($dbconn, $userId);
 } else {
-    $dbconn = $db->getConnection();
-    $user = User::constructSPWithUserId($dbconn, $userId);
-    $photos = $user->getPhotos();
+    if (isset($productId)) {
+        $dbconn = $db->getConnection();
+        $user = User::constructSPWithProductId($dbconn, $userId, $productId);
+        $photos = $user->getProductPhotos();
+    } else {
+        $dbconn = $db->getConnection();
+        $user = User::constructSPWithUserId($dbconn, $userId);
+        $photos = $user->getPhotos();
+    }
 }
 ?>
 
@@ -25,18 +33,27 @@ if (isset($productId)) {
 
 <head>
     <meta charset="UTF-8">
-    <title><?php echo "Profile | " . $user->getBusinessName(); ?>
+    <title>
+        <?php
+        if ($_SESSION['user_type'] == "customer") {
+            echo "Profile | " . $user->getFirstName() . " " . $user->getLastName();
+        } else {
+            echo "Profile | " . $user->getBusinessName();
+        }
+        ?>
     </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
+
 </head>
 
 <body>
+    <!--<div class="container mt-5" style="font-size: 1.1rem;">-->
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-3">
-                <img src="<?= $user->getProfileImage() ?>" alt="Profile Image_<?= $user->getFirstName() ?>" class="img-fluid rounded-circle">
-                <i class="bi bi-pencil-square position-absolute top-0 start-0 translate-middle bg-white rounded-circle p-2"></i>
+            <div class="col-md-3 text-center">
+                <img src="avatar1.jpg" alt="Profile Photo" class="img-fluid rounded-circle" style="width: 150px;height: 150px;border-radius: 50%;object-fit: cover;">
+                <img src="<?= $user->getProfileImage() ?>" alt="Profile Image_<?= $user->getFirstName() ?>" class="img-fluid rounded-circle" style="width: 150px;height: 150px;border-radius: 50%;object-fit: cover;">
             </div>
             <div class="col-md-9">
                 <!-- dont forget to put other usertypes' differentdata here -->
@@ -44,21 +61,22 @@ if (isset($productId)) {
                 //phone: should be in icon
                 //whatsapp icon leads to the whatsapp chat
                 //https://wa.me/94765907934  this link should be merge from the database
+                //buttons needs to be added
                 ?>
                 <h1 class="col-md-3 mb-3"><?= $user->getFirstName() ?> <?= $user->getLastName() ?></h1>
-                <h1 class="col-md-3 mb-3"><?= $user->getBusinessName() ?></h1>
+                <h1 class><?= $user->getBusinessName() ?></h1>
                 <p class="mb-2"><span class="mr-2">
                         <i class="bi bi-envelope mr-2"></i>
                     </span> <?= $user->getEmail() ?></p>
                 <p class="mb-2"><span class="mr-2">
                         <i class="bi bi-telephone mr-2"></i>
-                    </span> <a href="tel:+94'.$user->getPhone().'" target="_blank"><?= $user->getPhone() ?></a></p>
+                    </span> <a href="tel:+94" .$user->getPhone().'"><?= $user->getPhone() ?></a></p>
                 <p class="mb-2">
                     <span class="mr-2">
                         <i class="bi bi-whatsapp mr-2"></i>
                     </span>
                     <!-- check digits if need trim -->
-                    <a href="https://wa.me/'.$user->getWPhone().'" target="_blank"><?= $user->getWPhone() ?></a>
+                    <a href="https://wa.me/" .$user->getWPhone() target="_blank"><?= $user->getWPhone() ?></a>
                 </p>
                 <p class="mb-2">
                     <i class="bi bi-geo-alt mr-2"></i>
@@ -70,9 +88,12 @@ if (isset($productId)) {
                     </span>
                     <?= $user->getDescription() ?>
                 </p>
+                <button class="btn btn-primary">Edit</button>
+
+                <h2 class="mt-5">Reviews</h2>
                 <p class="mb-2">
                     <span class="mr-2">
-                        Reviews :
+                        <i class="bi bi-star mr-2"></i>
                     </span>
                     <span class="rating">
                         <span class="text-warning">&#9733;</span>
@@ -82,23 +103,26 @@ if (isset($productId)) {
                         <span class="text-warning">&#9734;</span> <!-- empty star for illustration -->
                     </span>
                 </p>
-                <button class="btn btn-primary">Edit</button>
             </div>
         </div>
-        <hr>
-        <div class="row">
-            <div class="col-12">
-                <h2>Photos</h2>
-                <div class="d-flex flex-wrap">
-                    <?php foreach ($photos as $photoPath) {
-                        echo '<div class="card m-2" style="width: 18rem;">';
-                        echo '<img src="' . $photoPath . '" class="card-img-top" alt="Image of ' . $userId . '">';
-                        echo '</div>';
-                    }
-                    ?>
-                </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-12">
+            <h2>Photos</h2>
+            <button class="btn btn-success mb-2">Add Photo</button>
+            <div class="d-flex flex-wrap">
+                <?php foreach ($photos as $photo) : ?>
+                    <div class="card m-2" style="width: 18rem;">
+                        <img src="<?= $photo ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <button class="btn btn-danger">Delete</button>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
+    </div>
     </div>
 </body>
 
