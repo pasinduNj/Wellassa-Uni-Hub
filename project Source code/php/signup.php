82f4710cfec684f8f09b77d1f926d7c48b27331b
Raees Ahamed <raeesahmedh116@gmail.com>
2024-08-04 14:18:29 +0530

    Update signup.php

--- project Source code/php/signup.php
+++ project Source code/php/signup.php
@@ -35,7 +35,7 @@ if (isset($_POST['userType'])) {
     $confirmPassword = trim($_POST['confirmPassword']);
     $firstName = sanitize_input($_POST['firstName']);
     $lastName = sanitize_input($_POST['lastName']);
-    $businessName = sanitize_input($_POST['businessName']);
+    $businessName = trim($_POST['businessName']);
     $nicNumber = sanitize_input($_POST['nicNumber']);
     $whatsappNumber = sanitize_input($_POST['whatsappNumber']);
     $serviceAddress = trim($_POST['serviceAddress']);
